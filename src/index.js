24bd6cfc5660aeb9439a0a8cfc2cc6f97e41b1c0
Yulia Starikova <yupipper@gmail.com>
2024-02-10 11:35:17 +0300

    test js btn search

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import "./styles/index.scss"; // для компиляции css?
 import "regenerator-runtime/runtime"; // для чего?
 import './Milia' // js Милии
 import './Elena' // js Лены
-import './yulia' // js Юлии
+import './Yulia' // js Юлии
