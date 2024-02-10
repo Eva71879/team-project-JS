060109ff8cc83262303958dab461d81abe59cf34
Yulia Starikova <yupipper@gmail.com>
2024-02-10 12:47:00 +0300

    add search-block

--- src/yulia.js
+++ src/yulia.js
@@ -1,5 +1,9 @@
 const btnSearch = document.getElementById('btnSearch');
+const basket = document.querySelector('.basket');
+const category = document.querySelector('.category');
+const inputSearch = document.querySelector('.inputSearch');
 
 btnSearch.addEventListener('click', function () {
-    console.log('mao');
+    category.classList.toggle('hide');
+    inputSearch.classList.toggle('hide');
 })
