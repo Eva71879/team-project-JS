0debf74ba240fcf21199cf1af6a01f452c3d046c
Kashinka <azalena@yandex.ru>
2024-02-17 15:57:27 +0300

    Corrected preloader

--- src/Alena.js
+++ src/Alena.js
@@ -1,4 +1,4 @@
-window.onload.addEventListener("DOMContentLoaded", ready) = function(){
+window.onload = function showPreloader(){
     let preloader = document.getElementById('preload');
     preloader.style.display = 'none';
 };
