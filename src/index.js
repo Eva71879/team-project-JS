0577e252cda8589572cc521c77f49039bab51ac9
Yulia Starikova <yupipper@gmail.com>
2024-02-17 19:58:36 +0300

    readme change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "./styles/index.scss"; // для компиляции css?
 import "regenerator-runtime/runtime"; // для чего?
 import './Milia' // js Милии
 import './Elena' // js Лены
-import './search-block' // js Юлии
+import './yulia' // js Юлии
 import './cart-button' //скрипт для Корзины
 import './Alena' //прелоадер
