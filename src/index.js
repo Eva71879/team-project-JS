a2e94fac13a477930892067d42ad86620e9aec04
Yulia Starikova <yupipper@gmail.com>
2024-02-17 19:32:18 +0300

    rename my codpart by functional block

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "./styles/index.scss"; // для компиляции css?
 import "regenerator-runtime/runtime"; // для чего?
 import './Milia' // js Милии
 import './Elena' // js Лены
-import './yulia' // js Юлии
+import './search-block' // js Юлии
 import './cart-button' //скрипт для Корзины
 import './Alena' //прелоадер
