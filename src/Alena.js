d4aae890d2c47591831e243b8063a431f8c1c0da
Elena <Eva71879@gmail.com>
2024-02-18 03:19:41 +0300

    build

--- src/Alena.js
+++
@@ -1,4 +0,0 @@
-window.onload = function showPreloader(){
-    let preloader = document.getElementById('preload');
-    preloader.style.display = 'none';
-};
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import "./styles/index.scss"; // для компиляции css
-import './Milia' // js Милии
-import './Elena' // js Лены
-import './yulia' // js Юлии
-import './cart-button' //скрипт для Корзины
-import './Alena' //прелоадер
